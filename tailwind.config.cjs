6e7e1d70c397103f3fe7bcad05d4c9d87e512a21
Gary Bleasdale <gary@bleasdale.dev>
2023-03-29 14:41:11 -0300

    hero section added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,12 @@ module.exports = {
     fontFamily: {
       sans: ['Inter var', ...defaultTheme.fontFamily.sans],
     },
+    extend:{
+      backgroundImage:{ 
+        'hero' : "url('/home/gmbleasdale/Desktop/Programming/Test/Pirapora_eComm/pirapora-ecomm/src/assets/hero_photo.jpeg')",
+        'hero-opacity-50': 'rgba(0, 0, 0, 0.5)'
+      }
+    }
   },
   plugins: [],
 }
