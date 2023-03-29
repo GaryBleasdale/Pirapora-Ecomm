ae47350bffb18147ef60d4199f6e4f5bd9ae9c76
Gary Bleasdale <gary@bleasdale.dev>
2023-03-29 16:20:12 -0300

    populated header data and changed colors

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -33,7 +33,7 @@ export default function Hero() {
               >
                 About us
               </a>
-              <a href="#" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              <a href="#" className="rounded-md bg-lime-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 Shop Now <span aria-hidden="true">→</span>
               </a>
             </div>
